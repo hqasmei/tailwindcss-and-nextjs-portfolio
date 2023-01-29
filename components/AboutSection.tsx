8f7b1b316d64485d2bf1ac099fa0f3a26ad201a9
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-01-29 11:58:01 -0800

    Fixed White space

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -18,7 +18,7 @@ const skills = [
 const AboutSection = () => {
   return (
     <section id="about">
-      <div className="my-12 pt-12 md:pt-16 md:pb-48">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
           About Me
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
