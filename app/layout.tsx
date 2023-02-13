74027fa267d44bf5caa819c44516ffd3091ed0f9
Yousef Abdelkhaleq <yousefbacarro@icloud.com>
2023-02-12 19:49:56 -0500

    Portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+"use client"
+import '../styles/globals.css';
+import Navbar from '@/components/Navbar';
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
@@ -12,7 +15,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ThemeProvider enableSystem={true} attribute="class">
+        <Navbar />
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
---
+++ components/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection</div>
+  )
+}
+
+export default HeroSection
