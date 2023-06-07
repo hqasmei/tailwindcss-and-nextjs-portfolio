1aa6181e9ead1bc479126ebf82d526e3e9d9df15
Kaito Hori <115637668+nbvrivnf@users.noreply.github.com>
2023-06-07 21:13:53 +0900

    Update Navbar.tsx
    name and add Contact nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,6 +25,10 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     page: "projects",
   },
+  {
+    label: "Contact",
+    page: "contact"
+  },
 ]
 
 export default function Navbar() {
@@ -39,7 +43,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Hosna Qasmei</h2>
+                <h2 className="text-2xl font-bold">Kaito Hori</h2>
               </div>
             </Link>
             <div className="md:hidden">
@@ -91,7 +95,7 @@ export default function Navbar() {
                   onClick={() => setTheme("dark")}
                   className="bg-slate-100 p-2 rounded-xl"
                 >
-                  <RiMoonFill size={25} />
+                  <RiMoonFill size={25} color="black" />
                 </button>
               )}
             </div>
