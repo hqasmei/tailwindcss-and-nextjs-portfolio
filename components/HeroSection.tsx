877996f3aeab985f949b589a8d0842f4c1a802d7
jimmyshoubaki <jimmyshoubaki@gmail.com>
2023-08-20 18:47:01 -0500

    Profile Picture Fix

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-3 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src="/headshot.jpg"
+            src="/myHeadshot.jpg"
             alt=""
             width={325}
             height={325}
