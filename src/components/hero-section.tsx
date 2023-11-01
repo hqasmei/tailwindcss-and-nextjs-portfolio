a650be698eab34c2b58910e28a9790b0fa824f80
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-01 00:19:47 -0700

    Testing, fixing bug

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
           offset={-100}
           duration={500}
         >
-          <HiArrowDown size={35} className="animate-bounce" />
+          {/* <HiArrowDown size={35} className="animate-bounce" /> */}
         </Link>
       </div>
     </section>
