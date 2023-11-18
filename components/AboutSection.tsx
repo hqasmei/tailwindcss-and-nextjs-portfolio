3320d5813eb05a5d83be85936eca5dde12637016
teambits009 <teambitsandy@gmail.com>
2023-11-19 00:58:11 +0300

    Home

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -20,26 +20,25 @@ const AboutSection = () => {
     <section id="about">
       <div className="my-12 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
-          About Me
+          About Brandon
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
         </h1>
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
           <div className="md:w-1/2 ">
             <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
-              Get to know me!
+              Lets get to know each other!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
+              Hi, my name is Brandon and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
               <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              based in Nairobi, Kenya.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
+              I pursued a course in Computer Science and I've been working in the
               field ever since.
             </p>
             <br />
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -18,13 +18,13 @@ const HeroSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Hosna!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Brandon!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               Software Engineer{" "}
             </span>
-            based in Los Angeles, CA. Working towards creating software that
+            based in Nairobi, Kenya. Working towards creating software that
             makes life easier and more meaningful.
           </p>
           <Link
