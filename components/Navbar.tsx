0a373fd2e52ae9869492015474ea23ac730a256d
Kin Learns <109691311+kinlearn@users.noreply.github.com>
2024-03-11 13:59:42 +0530

    Update Navbar.tsx
    Added `cursor-pointer` to navbar items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar() {
                     key={idx}
                     to={item.page}
                     className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
                     }
                     activeClass="active"
                     spy={true}
